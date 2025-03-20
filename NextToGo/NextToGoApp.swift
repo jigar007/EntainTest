61813c01b6880db5cf3aa6bc183bc560c15d65a8
Jigar Thakkar <ijigarthakkar@gmail.com>
2025-03-20 22:56:17 +1100

    Initial Commit

---
+++ NextToGo/NextToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToGoApp.swift
+//  NextToGo
+//
+//  Created by Jigar Thakkar on 20/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct NextToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
